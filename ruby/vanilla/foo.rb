0b166e5c4707dd81ab760fd8ad377436c023c35b
Neel Shah <neelshah.sa@gmail.com>
2022-04-27 14:17:17 +0200

    Repro SystemStackError ruby

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -1,8 +1,22 @@
 require 'sentry-ruby'
 require 'pry'
+require 'pry-nav'
 
 Sentry.init do |config|
   config.debug = true
+  config.before_send = lambda do |event, hint|
+    puts("Before send called")
+    puts event
+    event
+  end
 end
 
-Sentry.capture_message("testing default TLS")
+def recurse
+  recurse
+end
+
+begin
+  recurse
+rescue SystemStackError => exception
+  Sentry.capture_exception(exception, hint: { background: false })
+end
