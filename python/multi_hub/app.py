4d67b0fbfb03f0b5b12cde39d457355318517acb
Neel Shah <neelshah.sa@gmail.com>
2022-05-10 16:00:37 +0200

    Add multi hub py

---
+++ python/multi_hub/app.py
@@ -0,0 +1,20 @@
+import sentry_sdk
+
+DSN1 = "https://2fb45f003d054a7ea47feb45898f7649@o447951.ingest.sentry.io/5434472"
+DSN2 = "https://d655584d05f14c58b86e9034aab6817f@o447951.ingest.sentry.io/5461230"
+
+first_client = sentry_sdk.init(DSN1, debug=True)
+second_client = sentry_sdk.Hub(sentry_sdk.Client(DSN2, debug=True))
+
+try:
+    1 / 0
+except Exception as e:
+    sentry_sdk.capture_exception(e)
+
+
+with second_client:
+    l = {}
+    try:
+        x = l['error']
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
