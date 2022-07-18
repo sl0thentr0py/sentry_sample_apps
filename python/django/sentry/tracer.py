8803eea5d1567a1dd771b235e5888695abad3a5f
Neel Shah <neelshah.sa@gmail.com>
2022-07-18 20:44:37 +0200

    Prepare modes for prez

--- python/django/sentry/tracer.py
+++ python/django/sentry/tracer.py
@@ -1,9 +1,7 @@
 import sys
 
 def init_tracer():
-    from django.core.handlers.wsgi import WSGIHandler
-
-    old_app = WSGIHandler.__call__
+    MODE = 2
 
     def tracer(frame, event, arg):
         if event != 'call':
@@ -18,22 +16,28 @@ def init_tracer():
         caller_line_no = caller.f_lineno
         caller_filename = caller.f_code.co_filename
 
-        if ("sentry-sdk" not in func_filename or
-            "sentry-sdk" in func_filename and "sentry-sdk" in caller_filename):
-            return
+        if MODE == 2:
+            if ("sentry-sdk" not in func_filename or
+                "sentry-sdk" in func_filename and "sentry-sdk" in caller_filename):
+                return
 
         print(f"Calling {func_filename}:{func_line_no} {func_name} from:")
 
-        caller = frame
-        for i in range(5):
-            caller = caller.f_back
-            caller_name = caller.f_code.co_name
-            caller_line_no = caller.f_lineno
-            caller_filename = caller.f_code.co_filename
-            print(f"from {caller_filename}:{caller_line_no} {caller_name}")
+        if MODE == 2:
+            caller = frame
+            for i in range(5):
+                caller = caller.f_back
+                caller_name = caller.f_code.co_name
+                caller_line_no = caller.f_lineno
+                caller_filename = caller.f_code.co_filename
+                print(f"from {caller_filename}:{caller_line_no} {caller_name}")
 
-        print("="*50)
+            print("="*50)
+
         return
+
+    from django.core.handlers.wsgi import WSGIHandler
+    old_app = WSGIHandler.__call__
 
     def trace_patched_handler(self, environ, start_response):
         sys.settrace(tracer)
