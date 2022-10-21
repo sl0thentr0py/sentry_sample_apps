8278248801fe448a9f6949bb3f8aee9da588c277
Neel Shah <neelshah.sa@gmail.com>
2022-10-21 13:10:38 +0200

    Fix middleware ordering

--- ruby/sentry_rails_sample/config/initializers/01_sentry.rb
+++ ruby/sentry_rails_sample/config/initializers/01_sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.traces_sample_rate = 1.0
   config.debug = true
-  config.logger.level = ::Logger::DEBUG
+  config.logger.level = ::Logger::INFO
   config.instrumenter = :otel
   config.capture_exception_frame_locals = true
   config.release = "test-sessions-#{Time.now.utc}"
--- ruby/sentry_rails_sample/config/initializers/02_otel.rb
+++ ruby/sentry_rails_sample/config/initializers/02_otel.rb
@@ -5,3 +5,8 @@ OpenTelemetry::SDK.configure do |c|
   c.use_all
   c.add_span_processor(Sentry::OpenTelemetry::SpanProcessor.new)
 end
+
+Rails.application.config.middleware.move_after(
+  Sentry::Rails::CaptureExceptions,
+  OpenTelemetry::Instrumentation::Rack::Middlewares::TracerMiddleware
+)
