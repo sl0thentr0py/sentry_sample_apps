e2ed71e44c289cf323ad767c4efb33a3b7f1cccd
Neel Shah <neelshah.sa@gmail.com>
2023-02-07 17:12:32 +0100

    Try ruby before_send_transaction

--- ruby/sentry_rails_sample/config/initializers/sentry.rb
+++ ruby/sentry_rails_sample/config/initializers/sentry.rb
@@ -5,5 +5,11 @@ Sentry.init do |config|
   config.logger.level = ::Logger::INFO
   config.capture_exception_frame_locals = true
   config.release = "test-sessions-#{Time.now.utc}"
-  config.experiments.custom_measurements = true
+  config.before_send_transaction = lambda do |event, _h|
+    # filter out SQL queries from spans with sensitive data
+    event.spans.each do |span|
+      span[:description] = '<FILTERED>' if span[:op].start_with?('db')
+    end
+    event
+  end
 end
