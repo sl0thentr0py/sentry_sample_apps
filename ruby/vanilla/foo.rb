ec56e337265c1b851318334cff3ab8d6faf326d1
Neel Shah <neelshah.sa@gmail.com>
2023-03-23 18:05:56 +0100

    Test profiler threading behaviour
    stackprof / rb_profile_frames will record samples only from the thread
    Stackprof.start is called from

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -7,26 +7,36 @@ Sentry.init do |config|
   config.traces_sample_rate = 1.0
 end
 
-def foo
-  Sentry.with_child_span(op: 'foo') do |span|
-    10000.times { 2 + 2 }
-    sleep 0.2
-    print('foo')
-  end
+def t1
+  10000.times { 2 + 2 }
+  sleep 0.2
+  puts('t1')
 end
 
-def bar
-  Sentry.with_child_span(op: 'bar') do
-    sleep 0.5
-    foo
-    20000.times { 2 * 2 }
-    print('bar')
-  end
+def t2
+  transaction = Sentry.start_transaction(name: 'profiling')
+  Sentry.get_current_scope.set_span(transaction)
+
+  sleep 0.5
+  t1
+  20000.times { 2 * 2 }
+  puts('t2')
+
+  transaction.finish
+end
+
+def main
+  sleep 0.2
+  20000.times { 2 ** 2 }
+  puts('main')
 end
 
 
-transaction = Sentry.start_transaction(name: 'profiling')
-Sentry.get_current_scope.set_span(transaction)
-foo
-bar
-transaction.finish
+# transaction = Sentry.start_transaction(name: 'profiling')
+# Sentry.get_current_scope.set_span(transaction)
+
+threads = [Thread.new { t1 }, Thread.new { t2 }]
+main
+threads.each(&:join)
+
+# transaction.finish
