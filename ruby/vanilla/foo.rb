382eaf36bfbf1cafd38d7b819cb368fab39c6376
Neel Shah <neelshah.sa@gmail.com>
2023-03-23 17:51:14 +0100

    Testing ruby profiler

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -1,16 +1,32 @@
 require 'sentry-ruby'
-require 'pry'
-require 'pry-nav'
+require 'debug'
 
 Sentry.init do |config|
+  config.release = "profiling"
   config.debug = true
   config.traces_sample_rate = 1.0
 end
 
-
-transaction = Sentry.start_transaction(name: 'testing long desc')
-long_ass_string = (1..10000).map(&:to_s).join('-')
-transaction.with_child_span(description: long_ass_string) do
-  sleep(1)
+def foo
+  Sentry.with_child_span(op: 'foo') do |span|
+    10000.times { 2 + 2 }
+    sleep 0.2
+    print('foo')
+  end
 end
+
+def bar
+  Sentry.with_child_span(op: 'bar') do
+    sleep 0.5
+    foo
+    20000.times { 2 * 2 }
+    print('bar')
+  end
+end
+
+
+transaction = Sentry.start_transaction(name: 'profiling')
+Sentry.get_current_scope.set_span(transaction)
+foo
+bar
 transaction.finish
