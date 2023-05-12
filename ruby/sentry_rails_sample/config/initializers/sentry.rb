72138e6a26537f45d8801c02285b9b4adec248c3
Neel Shah <neelshah.sa@gmail.com>
2023-05-12 16:47:10 +0200

    Test http stuff

--- ruby/sentry_rails_sample/config/initializers/sentry.rb
+++ ruby/sentry_rails_sample/config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.traces_sample_rate = 1.0
-  config.profiles_sample_rate = 1.0
+  config.send_default_pii = true
   config.debug = true
   config.logger.level = ::Logger::DEBUG
   config.include_local_variables = true
