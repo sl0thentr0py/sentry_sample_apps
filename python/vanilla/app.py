9510d9b0d128ecb1f8279077157cec1c6ab9113a
Neel Shah <neelshah.sa@gmail.com>
2023-06-28 13:30:13 +0200

    nested tx experiments

--- python/vanilla/app.py
+++ python/vanilla/app.py
@@ -1,7 +1,31 @@
-import pprint
+import linecache
 import contextlib
 import tracemalloc
 import sentry_sdk
+from threading import Thread, get_ident
+
+sentry_sdk.init(traces_sample_rate=1.0, debug=True, send_default_pii=True)
+
+
+def display_top(snapshot, limit=25):
+    # just a helper for including the actual line of code instead of just
+    # filename + line number and making the output more human friendly
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(snapshot[:limit], 1):
+        frame = stat.traceback[0]
+        print(
+            "#%s: %s:%s: %.1f KiB"
+            % (
+                index,
+                frame.filename,
+                frame.lineno,
+                stat.size / 1024,
+            )
+        )
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print("    %s" % line)
+
 
 @contextlib.contextmanager
 def measure():
@@ -16,13 +40,22 @@ def measure():
 
     snapshot2 = tracemalloc.take_snapshot().filter_traces(filters)
     snapshot = snapshot2.compare_to(snapshot1, 'lineno')
-    pprint.pprint(snapshot)
+    display_top(snapshot)
 
 
 sentry_sdk.init()
 
+def work():
+    for i in range(10**3):
+        print("%d: %d" % (get_ident(), i))
+        with sentry_sdk.start_transaction(name='child transaction'):
+            for i in range(1050):
+                with sentry_sdk.start_span(op='sleep', description='zzz'):
+                    2**10
+
+
 with measure():
-    try:
-        1 / 0
-    except Exception:
-        sentry_sdk.capture_exception()
+    for i in range(5):
+        Thread(target=work).run()
+
+sentry_sdk.flush(10)
