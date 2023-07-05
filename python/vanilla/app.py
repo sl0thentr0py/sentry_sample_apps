dd3620c580a65263a3bd7efddace26b5501b2ec7
Neel Shah <neelshah.sa@gmail.com>
2023-07-05 15:08:07 +0200

    Testing new backpressure stuff

--- python/vanilla/app.py
+++ python/vanilla/app.py
@@ -1,61 +1,44 @@
-import linecache
-import contextlib
-import tracemalloc
+from time import sleep, time
 import sentry_sdk
-from threading import Thread, get_ident
-
-sentry_sdk.init(traces_sample_rate=1.0, debug=True, send_default_pii=True)
 
 
-def display_top(snapshot, limit=25):
-    # just a helper for including the actual line of code instead of just
-    # filename + line number and making the output more human friendly
-    print("Top %s lines" % limit)
-    for index, stat in enumerate(snapshot[:limit], 1):
-        frame = stat.traceback[0]
-        print(
-            "#%s: %s:%s: %.1f KiB"
-            % (
-                index,
-                frame.filename,
-                frame.lineno,
-                stat.size / 1024,
-            )
-        )
-        line = linecache.getline(frame.filename, frame.lineno).strip()
-        if line:
-            print("    %s" % line)
+sentry_sdk.init(traces_sample_rate=1.0, _experiments={"enable_backpressure_handling": True})
+
+def size():
+    q = sentry_sdk.Hub.current.client.transport._worker._queue
+    print(f"size: {q.qsize()}, full: {q.full()}")
 
 
-@contextlib.contextmanager
-def measure():
-    filters = [tracemalloc.Filter(True, '**sentry_sdk**')]
+last = time()
+times = []
 
-    if not tracemalloc.is_tracing():
-        tracemalloc.start()
-
-    snapshot1 = tracemalloc.take_snapshot().filter_traces(filters)
-
-    yield
-
-    snapshot2 = tracemalloc.take_snapshot().filter_traces(filters)
-    snapshot = snapshot2.compare_to(snapshot1, 'lineno')
-    display_top(snapshot)
-
-
-sentry_sdk.init()
-
-def work():
-    for i in range(10**3):
-        print("%d: %d" % (get_ident(), i))
-        with sentry_sdk.start_transaction(name='child transaction'):
-            for i in range(1050):
+print("running heavy load")
+for i in range(50):
+    for j in range(1000):
+        with sentry_sdk.start_transaction(name='backpressure'):
+            for i in range(10):
                 with sentry_sdk.start_span(op='sleep', description='zzz'):
-                    2**10
+                    pass
+    now = time()
+    times.append(now - last)
+    last = now
 
+print(times)
+print("waiting for stuff to clear")
+sleep(5)
 
-with measure():
-    for i in range(5):
-        Thread(target=work).run()
+times = []
+print("running normal load")
+for i in range(10):
+    for j in range(50):
+        with sentry_sdk.start_transaction(name='backpressure'):
+            for i in range(10):
+                with sentry_sdk.start_span(op='sleep', description='zzz'):
+                    pass
+    now = time()
+    times.append(now - last)
+    last = now
+    sleep(5)
 
+print(times)
 sentry_sdk.flush(10)
