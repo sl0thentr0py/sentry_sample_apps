f4d8660e260d84ff7d470775eabaed67edd92455
Neel Shah <neelshah.sa@gmail.com>
2023-09-27 16:17:46 +0200

    Test ruby crons

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -2,41 +2,13 @@ require 'sentry-ruby'
 require 'debug'
 
 Sentry.init do |config|
-  config.release = "profiling"
+  config.release = "crons-testing"
   config.debug = true
   config.traces_sample_rate = 1.0
 end
 
-def t1
-  10000.times { 2 + 2 }
-  sleep 0.2
-  puts('t1')
-end
+schedule = Sentry::Cron::MonitorSchedule::Crontab.new("blablabla")
+config = Sentry::Cron::MonitorConfig.new(schedule)
+Sentry.capture_check_in("crontab_bad_test", :ok, monitor_config: config)
 
-def t2
-  transaction = Sentry.start_transaction(name: 'profiling')
-  Sentry.get_current_scope.set_span(transaction)
-
-  sleep 0.5
-  t1
-  20000.times { 2 * 2 }
-  puts('t2')
-
-  transaction.finish
-end
-
-def main
-  sleep 0.2
-  20000.times { 2 ** 2 }
-  puts('main')
-end
-
-
-# transaction = Sentry.start_transaction(name: 'profiling')
-# Sentry.get_current_scope.set_span(transaction)
-
-threads = [Thread.new { t1 }, Thread.new { t2 }]
-main
-threads.each(&:join)
-
-# transaction.finish
+sleep 1
