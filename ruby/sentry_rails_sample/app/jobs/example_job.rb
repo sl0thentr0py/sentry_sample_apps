0769d1341502db860ba80cf75b1b297d90c46cb6
Neel Shah <neelshah.sa@gmail.com>
2023-10-05 16:12:22 +0200

    use module

--- ruby/sentry_rails_sample/app/jobs/example_job.rb
+++ ruby/sentry_rails_sample/app/jobs/example_job.rb
@@ -1,8 +1,10 @@
 class ExampleJob < ApplicationJob
-  queue_as :default
+  include Sentry::Cron::MonitorCheckIns
 
-  def perform(*args)
-    HTTParty.get('https://www.google.com')
-    User.all.to_a
+  sentry_monitor_check_ins slug: 'custom_slug', monitor_config: Sentry::Cron::MonitorConfig.from_interval(1, :minute)
+
+  def perform(site, user: true)
+    HTTParty.get(site)
+    User.all.to_a if user
   end
 end
