fd71ef32def084702086767a0a8d388d3196ae62
Neel Shah <neelshah.sa@gmail.com>
2023-11-15 13:57:23 +0100

    Try auto sidekiq-cron

--- ruby/sentry_rails_sample/app/jobs/example_job.rb
+++ ruby/sentry_rails_sample/app/jobs/example_job.rb
@@ -1,4 +1,8 @@
 class ExampleJob < ApplicationJob
+  # include Sentry::Cron::MonitorCheckIns
+
+  # sentry_monitor_check_ins monitor_config: Sentry::Cron::MonitorConfig.from_crontab("* 2 * * *")
+
   def perform(site, user: true)
     HTTParty.get(site)
     User.all.to_a if user
--- ruby/sentry_rails_sample/app/jobs/outer_long_ass_name/inner_long_ass_name_long_ass_name_long_ass_name/sidekiq_job.rb
+++ ruby/sentry_rails_sample/app/jobs/outer_long_ass_name/inner_long_ass_name_long_ass_name_long_ass_name/sidekiq_job.rb
@@ -2,9 +2,9 @@ module OuterLongAssName
   module InnerLongAssNameLongAssNameLongAssName
     class SidekiqJob
       include Sidekiq::Job
-      include Sentry::Cron::MonitorCheckIns
+      # include Sentry::Cron::MonitorCheckIns
 
-      sentry_monitor_check_ins monitor_config: Sentry::Cron::MonitorConfig.from_crontab("0 * * * *")
+      # sentry_monitor_check_ins monitor_config: Sentry::Cron::MonitorConfig.from_crontab("*/5 * * * *")
 
       def perform(site, user = true)
         HTTParty.get(site)
