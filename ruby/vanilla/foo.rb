552f13cb30af326e00f4891212a9d9acdfda9035
Neel Shah <neelshah.sa@gmail.com>
2023-12-07 16:46:42 +0100

    test ruby backpressure

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -2,12 +2,49 @@ require 'sentry-ruby'
 require 'debug'
 
 Sentry.init do |config|
-  config.release = "crons-testing"
-  config.debug = true
+  config.release = 'backpressure-testing'
   config.traces_sample_rate = 1.0
+  config.logger.level = :warn
+  config.enable_backpressure_handling = true
 end
 
-config = Sentry::Cron::MonitorConfig.from_interval(300, :minute)
-Sentry.capture_check_in("Crontab_300", :ok, monitor_config: config)
 
-sleep 1
+last = Time.now
+times = []
+
+puts 'running heavy load'
+
+50.times do
+  1000.times do
+    transaction = Sentry.start_transaction(name: 'backpressure')
+    10.times { transaction.with_child_span(op: 'sleep', description: 'zzz') {} }
+    transaction.finish
+  end
+
+  puts "queue: #{Sentry.background_worker.instance_variable_get(:@executor).queue_length}"
+
+  now = Time.now
+  times.append(now - last)
+  last = now
+end
+
+puts times.join(' ')
+puts 'waiting for stuff to clear'
+sleep(5)
+
+times = []
+print 'running normal load'
+
+10.times do
+  50.times do
+    transaction = Sentry.start_transaction(name: 'backpressure')
+    10.times { transaction.with_child_span(op: 'sleep', description: 'zzz') {} }
+    transaction.finish
+  end
+
+  now = Time.now
+  times.append(now - last)
+  last = now
+end
+
+puts times.join(' ')
