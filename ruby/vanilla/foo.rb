d966a9fc4b41e5d1f932b8aa424b1bba040f919f
Neel Shah <neelshah.sa@gmail.com>
2023-12-20 16:44:55 +0100

    test ruby flush client reports

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -2,49 +2,10 @@ require 'sentry-ruby'
 require 'debug'
 
 Sentry.init do |config|
-  config.release = 'backpressure-testing'
-  config.traces_sample_rate = 1.0
-  config.logger.level = :warn
-  config.enable_backpressure_handling = true
+  config.logger = Logger.new($stdout)
 end
 
+transport = Sentry.get_current_client.transport
 
-last = Time.now
-times = []
-
-puts 'running heavy load'
-
-50.times do
-  1000.times do
-    transaction = Sentry.start_transaction(name: 'backpressure')
-    10.times { transaction.with_child_span(op: 'sleep', description: 'zzz') {} }
-    transaction.finish
-  end
-
-  puts "queue: #{Sentry.background_worker.instance_variable_get(:@executor).queue_length}"
-
-  now = Time.now
-  times.append(now - last)
-  last = now
-end
-
-puts times.join(' ')
-puts 'waiting for stuff to clear'
-sleep(5)
-
-times = []
-print 'running normal load'
-
-10.times do
-  50.times do
-    transaction = Sentry.start_transaction(name: 'backpressure')
-    10.times { transaction.with_child_span(op: 'sleep', description: 'zzz') {} }
-    transaction.finish
-  end
-
-  now = Time.now
-  times.append(now - last)
-  last = now
-end
-
-puts times.join(' ')
+100.times { transport.record_lost_event(:ratelimit_backoff, "event") }
+40.times { transport.record_lost_event(:backpressure, "transaction") }
