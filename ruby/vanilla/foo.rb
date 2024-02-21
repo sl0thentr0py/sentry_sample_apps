628a4a375b75636e2bfabc051b2e6115a5db4fc3
Neel Shah <neelshah.sa@gmail.com>
2024-02-21 16:07:01 +0100

    testing metrics

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -4,17 +4,22 @@ require 'debug'
 Sentry.init do |config|
   config.debug = true
   config.logger.level = ::Logger::DEBUG
+  config.enable_metrics = true
+  config.release = "testing-metrics"
 end
 
-10.times do |i|
-  crumb = Sentry::Breadcrumb.new(
-    data: { foo: 42 },
-    category: 'crumb',
-    message: "crumb no: #{i}"
-  )
+3.times { Sentry::Metrics.incr('foo') }
+3.times { Sentry::Metrics.incr('bar') }
 
-  Sentry.add_breadcrumb(crumb)
-end
+sleep 2
 
-config = Sentry::Cron::MonitorConfig.from_interval(1, :minute)
-Sentry.capture_check_in('test-scope', :in_progress, monitor_config: config)
+10.times { Sentry::Metrics.distribution('dist', Random.rand, 'millisecond', tags: { unicode: 'snöwmän', fortytwo: 42 }) }
+100.times { Sentry::Metrics.gauge('gauge', Random.rand, 'ratio', tags: { 'normal' => 'cow', 'normalize snöwmän' => 'potato' }) }
+50.times { Sentry::Metrics.set('settt', [1, 2, 3].sample, 'second') }
+
+sleep 5
+
+2.times { Sentry::Metrics.incr('foo') }
+7.times { Sentry::Metrics.incr('bar') }
+
+sleep 10
