9938e5dcf6fe49d32061e78b0a25f867a05b2932
Neel Shah <neelshah.sa@gmail.com>
2024-02-26 16:12:27 +0100

    test metric with tx

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -5,21 +5,14 @@ Sentry.init do |config|
   config.debug = true
   config.logger.level = ::Logger::DEBUG
   config.enable_metrics = true
+  config.enable_tracing = true
   config.release = "testing-metrics"
 end
 
-3.times { Sentry::Metrics.incr('foo') }
-3.times { Sentry::Metrics.incr('bar') }
-
-sleep 2
-
-10.times { Sentry::Metrics.distribution('dist', Random.rand, 'millisecond', tags: { unicode: 'snöwmän', fortytwo: 42 }) }
-100.times { Sentry::Metrics.gauge('gauge', Random.rand, 'ratio', tags: { 'normal' => 'cow', 'normalize snöwmän' => 'potato' }) }
-50.times { Sentry::Metrics.set('settt', [1, 2, 3].sample, 'second') }
-
-sleep 5
-
-2.times { Sentry::Metrics.incr('foo') }
-7.times { Sentry::Metrics.incr('bar') }
+transaction = Sentry.start_transaction(name: 'metrics tx')
+Sentry.get_current_scope.set_span(transaction)
+10.times { Sentry::Metrics.incr('foo') }
+100.times { Sentry::Metrics.gauge('gauge', Random.rand, unit: 'ratio', tags: { 'with' => 'transaction' }) }
+transaction.finish
 
 sleep 10
