4bb174f3d1e80fc66e4230de9fc8c51981587d6b
Neel Shah <neelshah.sa@gmail.com>
2024-08-13 17:10:33 +0200

    latest potel testing

--- python/vanilla/app.py
+++ python/vanilla/app.py
@@ -8,24 +8,28 @@ sentry_sdk.init(
     _experiments={"otel_powered_performance": True},
 )
 
-tracer = trace.get_tracer(__name__)
+
+sentry_sdk.set_tag("tag.global", 99)
 
 ## sentry apis
-# with sentry_sdk.start_span(description="sentry request"):
-#     sleep(0.1)
-#     with sentry_sdk.start_span(description="sentry db"):
-#         sleep(0.5)
-#         with sentry_sdk.start_span(description="sentry redis"):
-#             sleep(0.2)
-#     with sentry_sdk.start_span(description="sentry http"):
-#         sleep(1)
+with sentry_sdk.start_span(description="sentry request"):
+    sentry_sdk.set_tag("tag.inner", "asd")
+    sleep(0.1)
+    with sentry_sdk.start_span(description="sentry db"):
+        sleep(0.5)
+        with sentry_sdk.start_span(description="sentry redis"):
+            sleep(0.2)
+    with sentry_sdk.start_span(description="sentry http"):
+        sleep(1)
 
 ## otel apis
-with tracer.start_as_current_span("request"):
-    sleep(0.1)
-    with tracer.start_as_current_span("db"):
-        sleep(0.5)
-        with tracer.start_as_current_span("redis"):
-            sleep(0.2)
-    with tracer.start_as_current_span("http"):
-        sleep(1)
+# tracer = trace.get_tracer(__name__)
+
+# with tracer.start_as_current_span("request"):
+#     sleep(0.1)
+#     with tracer.start_as_current_span("db"):
+#         sleep(0.5)
+#         with tracer.start_as_current_span("redis"):
+#             sleep(0.2)
+#     with tracer.start_as_current_span("http"):
+#         sleep(1)
