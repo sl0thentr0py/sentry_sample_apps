a6e5b8b2b1df7806139d847de24d5cc4bab497ba
Neel Shah <neelshah.sa@gmail.com>
2024-09-20 14:28:47 +0200

    test vernier ruby

--- ruby/sentry_rails_sample/config/initializers/sentry.rb
+++ ruby/sentry_rails_sample/config/initializers/sentry.rb
@@ -1,6 +1,10 @@
+require 'sentry/vernier/profiler'
+
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger, :redis_logger]
   config.traces_sample_rate = 1.0
+  config.profiles_sample_rate = 1.0
+  config.profiler_class = Sentry::Vernier::Profiler
   config.send_default_pii = true
   config.logger.level = ::Logger::DEBUG
   config.include_local_variables = true
