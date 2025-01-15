61ab19cf489f514c7e44d6c0daeac8663ce6a576
Neel Shah <neelshah.sa@gmail.com>
2025-01-15 16:38:01 +0100

    multiprocessing deprecation python

--- python/vanilla/app.py
+++ python/vanilla/app.py
@@ -1,22 +1,20 @@
-import sentry_sdk
-from opentelemetry import trace
-from time import sleep
+import logging
+import multiprocessing
+import concurrent.futures
+import sentry_sdk.integrations.logging
+
+logging.captureWarnings(True)
+logging.basicConfig(level=logging.DEBUG)
 
 sentry_sdk.init(
-    debug=True,
     traces_sample_rate=1.0,
+    integrations=[
+        sentry_sdk.integrations.logging.LoggingIntegration(event_level=logging.WARNING),
+    ]
 )
+sentry_sdk.capture_message("hello")
 
-
-with sentry_sdk.isolation_scope() as isolation_scope:
-    print(sentry_sdk.get_isolation_scope()._tags)
-    sentry_sdk.set_tag("a", 42)
-    print(sentry_sdk.get_isolation_scope()._tags)
-    print(isolation_scope._tags)
-    with sentry_sdk.new_scope() as new_scope:
-        new_scope.set_tag("b", 21)
-        sentry_sdk.set_tag("c", "Asd")
-        print(new_scope._tags)
-        print(sentry_sdk.get_current_scope()._tags)
-        print(sentry_sdk.get_isolation_scope()._tags)
-        print(isolation_scope._tags)
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
+    pool = concurrent.futures.ProcessPoolExecutor()
+    pool.submit(sentry_sdk.capture_message, "world")
