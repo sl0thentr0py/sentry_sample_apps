e4ef74591a71bec94bcba5c2e4656d918c242d31
Neel Shah <neel.shah@sentry.io>
2025-03-13 16:34:39 +0100

    Try vips error exclude repro

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -1,10 +1,22 @@
 require 'sentry-ruby'
 require 'debug'
+require 'image_processing/vips'
 
 Sentry.init do |config|
-  config.metrics.enabled = true
-  config.metrics.bla = 42
+  config.logger.level = :debug
+  config.excluded_exceptions += ['Vips::Error']
 end
 
-Sentry::Metrics.incr(42)
-Sentry::Metrics.foobar('foo', unit: 'asdas')
+begin
+  ImageProcessing::Vips::Processor.call(
+    source: 'sample1.jfiadsadf',
+    loader: { page: 0 },
+    operations: [[:resize_to_limit, [200, 200]]],
+    saver: {},
+    destination: 'output.jfif'
+  )
+rescue => e
+  Sentry.capture_exception(e)
+end
+
+sleep 2
