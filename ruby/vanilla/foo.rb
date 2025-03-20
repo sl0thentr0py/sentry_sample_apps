7ee96ee31baa8e16cfa5e773ccccacdb17789d33
Neel Shah <neel.shah@sentry.io>
2025-03-20 15:52:46 +0100

    Try ruby threaded spans

--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -3,11 +3,20 @@ require 'debug'
 
 Sentry.init do |config|
   config.logger.level = :debug
+  config.traces_sample_rate = 1.0
 end
 
-e = Sentry::Event.new(configuration: Sentry.configuration)
-b = Sentry::BreadcrumbBuffer.new
-b.record(Sentry::Breadcrumb.new(message: "\x76\xab\x99\xb5"))
-e.breadcrumbs = b
+transaction = Sentry.start_transaction(name: 'transaction')
+Sentry.get_current_scope.set_span(transaction)
 
-Sentry.send_event(e, background: false)
+Sentry.with_child_span(op: 'child_same_thread') do
+  Thread.new do
+    Sentry.with_child_span(op: 'child_new_thread') do
+      sleep 0.3
+    end
+  end
+
+  sleep 0.5
+end
+
+transaction.finish
