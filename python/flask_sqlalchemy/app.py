b428c5758f9808014ad6c6b41bde52ac8046d81c
Neel Shah <neel.shah@sentry.io>
2025-05-27 18:12:21 +0200

    tracemalloc tracking flask

--- python/flask_sqlalchemy/app.py
+++ python/flask_sqlalchemy/app.py
@@ -1,7 +1,12 @@
+import pympler
 import sentry_sdk
+import opentelemetry
 from sentry_sdk.transport import HttpTransport
 import requests
 import time
+import linecache
+import tracemalloc
+import gc
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -28,11 +33,35 @@ def traces_sampler(sampling_context):
         return 1.0
 
 
+def display_top(snapshot, key_type='lineno', limit=5):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(True, "*sentry_sdk/opentelemetry/span_processor*"),
+        tracemalloc.Filter(True, "*opentelemetry*"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, frame.filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+
 sentry_sdk.init(
     release=f"flask-sqlalchemy-{sentry_sdk.VERSION}",
     # transport=FileWriteHttpTransport,
     debug=True,
-    # traces_sample_rate=1.0,
+    traces_sample_rate=1.0,
     # profiles_sample_rate=0.1,
 )
 
@@ -42,6 +71,24 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
+# tracemalloc.start()
+
+
+def track_memory(func):
+    def wrapper(*args, **kwargs):
+        snapshot1 = tracemalloc.take_snapshot()
+        display_top(snapshot1)
+
+        result = func(*args, **kwargs)
+
+        gc.collect()
+
+        snapshot2 = tracemalloc.take_snapshot()
+        display_top(snapshot2)
+        # top_stats = snapshot2.compare_to(snapshot1, "lineno") 
+
+        return result
+    return wrapper
 
 
 class User(db.Model):
@@ -61,6 +108,7 @@ def expensive():
         math.sqrt(64*64*64*64*64)
 
 @app.route("/count")
+# @track_memory
 def count():
     sentry_sdk.set_user({"email":"jane.doe@adas.com"})
     sentry_sdk.set_tag("foo", 42)
@@ -69,9 +117,8 @@ def count():
 
     with sentry_sdk.start_span(name="sleep") as span:
         span.set_data("span_foo", 23)
-        time.sleep(1)
-    requests.get("http://localhost:3000/error")
-    1/0
+        time.sleep(0.1)
+
     return f"<p>count: {count} </p>"
 
 
