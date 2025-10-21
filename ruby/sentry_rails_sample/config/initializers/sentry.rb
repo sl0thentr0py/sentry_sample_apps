8b528f23e12981b928a8ae6c4edbff2c050e77f3
Neel Shah <neel.shah@sentry.io>
2025-10-21 16:28:50 +0200

    test major

--- ruby/sentry_rails_sample/config/initializers/sentry.rb
+++ ruby/sentry_rails_sample/config/initializers/sentry.rb
@@ -1,11 +1,12 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger, :redis_logger]
-  # config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 1.0
+  config.profiles_sample_rate = 1.0
+  config.profiler_class = Sentry::Vernier::Profiler
+  config.enable_logs = true
   config.send_default_pii = true
-  config.logger.level = ::Logger::DEBUG
+  config.sdk_logger.level = ::Logger::DEBUG
   config.include_local_variables = true
   config.release = "test-neel-#{Time.now.utc}"
   config.enabled_patches << :graphql
-
-  config.sidekiq.propagate_traces = false unless Rails.const_defined?('Server')
 end
--- ruby/vanilla/foo.rb
+++ ruby/vanilla/foo.rb
@@ -3,7 +3,20 @@ require 'debug'
 
 Sentry.init do |config|
   config.sdk_logger.level = :debug
-  config.metrics.enabled = true
 end
 
-Sentry::Metrics.increment('foo')
+def recurse(n=0)
+  recurse(n+1)
+end
+
+def caller
+  recurse
+end
+
+begin
+  caller
+rescue Exception => e
+  Sentry.capture_exception(e)
+end
+
+sleep(2)
