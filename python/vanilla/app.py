184cca5359cf1b1a76026e8acbc9a947109eeb5d
Neel Shah <neelshah.sa@gmail.com>
2025-12-12 13:19:07 +0100

    Demonstrate otel span processor status python

--- python/vanilla/app.py
+++ python/vanilla/app.py
@@ -1,13 +1,30 @@
+import time
 import sentry_sdk
-from sentry_sdk.consts import SPANSTATUS
+from opentelemetry import trace
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.trace import Status, StatusCode
+from opentelemetry.sdk.trace import TracerProvider
+from sentry_sdk.integrations.opentelemetry import SentrySpanProcessor, SentryPropagator
 
 sentry_sdk.init(
     traces_sample_rate=1.0,
     debug=True,
+    instrumenter="otel",
 )
 
-with sentry_sdk.start_transaction(name="test"):
-    with sentry_sdk.start_span(op="foo", description="bar") as span:
-        span.set_status(SPANSTATUS.DEADLINE_EXCEEDED)
+provider = TracerProvider()
+provider.add_span_processor(SentrySpanProcessor())
+trace.set_tracer_provider(provider)
+set_global_textmap(SentryPropagator())
+tracer = trace.get_tracer(__name__)
+
+with tracer.start_as_current_span("tx") as tx:
+    status = Status(status_code=StatusCode.ERROR)
+    tx.set_status(status)
+
+    with tracer.start_as_current_span("span") as span:
+        status = Status(status_code=StatusCode.ERROR)
+        span.set_status(status)
+        time.sleep(3)
 
 sentry_sdk.flush()
